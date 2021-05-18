b08d677488bb2d9064bf60ee232bd5dae291f646
thomashayama <thomashayama@gmail.com>
2021-05-18 07:45:23 +0000

    testing detectron2

---
+++ detectron2/detectron2_test.py
@@ -0,0 +1,38 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+import os, json, cv2, random
+
+import detectron2
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+im = cv2.imread("./input.jpg")
+plt.figure()
+plt.imshow(im)
+print(type(im))
+plt.show()
+
+cfg = get_cfg()
+cfg.MODEL.DEVICE = 'cpu'
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+
+print(outputs["instances"].pred_classes)
+print(outputs["instances"].pred_boxes)
+
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+print(out.get_image()[:, :, ::-1])
+cv2.imwrite("test.png", out.get_image()[:, :, ::-1])
+
+# python detectron2_test.py
