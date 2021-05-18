1a4de3e34c6adc7c11832509eab7b25d0edd8d99
thomashayama <thomashayama@gmail.com>
2021-05-18 08:27:36 +0000

    testing detectron2 with wildcam data

---
+++ detectron2_test.py
@@ -0,0 +1,41 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+import os, json, cv2, random
+
+import detectron2
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+data_path = "/home/appuser/data/wildcamdata/iwildcam2021-fgvc8/train"
+files = os.listdir(data_path)
+print("Total Training Files:", len(files))
+print(files[:10])
+
+file = "8a00e048-21bc-11ea-a13a-137349068a90"
+im = cv2.imread(os.path.join(data_path, file + ".jpg"))
+
+cfg = get_cfg()
+cfg.MODEL.DEVICE = 'cpu'
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+
+print(outputs["instances"].pred_classes)
+print(outputs["instances"].pred_boxes)
+
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+#print(out.get_image()[:, :, ::-1])
+cv2.imwrite(file + ".jpg", im)
+cv2.imwrite(file + "_annotated.jpg", out.get_image()[:, :, ::-1])
+
+# python detectron2_test.py
